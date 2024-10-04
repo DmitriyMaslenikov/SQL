096979f8829cd75d6c70383ab4666c3784700c53
DmitriyMaslenikov <dmitriymaslenikov@gmail.com>
2024-10-04 16:29:19 +0300

    JOIN

--- library.sql
+++ library.sql
@@ -68,3 +68,13 @@ INSERT INTO books (id, name, ISBN, genre_id ) VALUES (gen_random_uuid(),'Death i
 
 INSERT INTO author_book_connection (id, author_id, book_id ) VALUES (gen_random_uuid(),'8b74163f-a8ce-4c9b-8569-144e652c1191', '4c2e68a0-8e79-4796-a49f-1e141e1ad674');
 INSERT INTO author_book_connection (id, author_id, book_id ) VALUES (gen_random_uuid(),'d354907b-7daf-4199-abf6-f73c53193a62', '2c737664-81fd-4aaa-8941-b22f22f46bd0');
+INSERT INTO author_book_connection (id, author_id, book_id ) VALUES (gen_random_uuid(),'314ae72d-7210-415a-8628-f394c253b07c', 'b5a66b4f-502e-4e02-a7ab-47ec9a29a19e');
+INSERT INTO author_book_connection (id, author_id, book_id ) VALUES (gen_random_uuid(),'85441b0d-6b7c-4c23-84b4-63de93293e33', 'b5a66b4f-502e-4e02-a7ab-47ec9a29a19e');
+INSERT INTO author_book_connection (id, author_id, book_id ) VALUES (gen_random_uuid(),'85441b0d-6b7c-4c23-84b4-63de93293e33', 'c8735262-abcb-4a37-9210-f5c60f930266');
+INSERT INTO author_book_connection (id, author_id, book_id ) VALUES (gen_random_uuid(),'8b74163f-a8ce-4c9b-8569-144e652c1191', 'c8735262-abcb-4a37-9210-f5c60f930266');
+
+
+SELECT * FROM books WHERE name = 'White acacia' OR name = 'Hot summer'
+
+SELECT books.name,isbn ,genres.genre FROM books  JOIN genres ON genres.id = books.genre_id
+WHERE name = 'White acacia'
